b8ad4c35e013482f6ff4190d9981e3c5a2f3a0a1
nabeela <nabans1811@gmail.com>
2016-03-21 18:07:27 +0530

    new code

--- application/config/database.php
+++ application/config/database.php
@@ -70,14 +70,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
+if($_SERVER['HTTP_HOST']=="localhost"){
+$password="";
+$username="root";
+}
+if($_SERVER['HTTP_HOST']=="junctiondev.cloudapp.net"){
+$password="bitnami";
+$username="root";	
+}
+
+
 $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
+	'username' => $username,
+	'password' => $password,
 	'database' => 'schoolweb',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
@@ -94,3 +104,4 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
