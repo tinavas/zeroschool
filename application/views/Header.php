5f90b7af9bcb97bf0795c2c5dbc17ad8ac867b96
nabeela <nabans1811@gmail.com>
2016-04-11 14:52:23 +0530

    change in code

--- application/views/Header.php
+++ application/views/Header.php
@@ -72,7 +72,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
               
               </ul>
             </li>
-			     <li><a href="<?=base_url();?>Front/About">Company</a></li>
+			
+			 <li><a href="http://junctiondev.cloudapp.net/appmanager/login/registration_application?id=School">Register and Use free</a></li>
+				  
+			 <li><a href="<?=base_url();?>Front/About">Company</a></li>
 				  
             <li><a href="<?=base_url();?>Front/Contactus">Contact us</a></li>
           </ul>
